a211e812d246e2cc3500ef5dafcc37261f447d2f
Gordana Grujicic <goxyu@yahoo.fr>
2021-03-11 17:38:48 +0100

    push

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -35,6 +35,56 @@ class FormController extends AbstractController
        }
     
         return $this->render('contact.html.twig' , ['formulaire' => $form->createView()]);
+        }
 
-    }
+
+
+        /**
+        * @Route("/contactlist", name="contactlist" )
+        */
+        public function show(Request $request)
+        {
+        // get Repository va aller au niveau des données dans la table précisée
+        // SELECT query
+        $repository = $this->getDoctrine()->getRepository(Contact::class);
+        // a ce stade il a accès au données
+        // je veux stocker dans la variable $contacts TOUT mes contacts
+        $contacts = $repository->findAll();
+
+
+        return $this->render('contactlist.html.twig', ['contacts'=>$contacts]);
+        }//entre guillement c'est le nom utillisé dans le TWIG
+        //avec $ c'est utilisé sur le Controller
+
+        /**
+         * @Route("/contact/edit/{id<\d+>}")
+         */
+        public function edit(Request $request, Contact $contact)
+        {
+        $form = $this->createForm(ContactType::class, $contact);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+        // va effectuer la requête d'UPDATE en base de données
+        $this->getDoctrine()->getManager()->flush();
+        }
+
+         return $this->render('contact.html.twig', ['formulaire'=>$form->createView()]);
+        }
+
+
+        /**
+        * @Route("/contact/delete/{id<\d+>}")
+        */
+        public function delete(Request $request, Contact $contact)
+        {
+        $em = $this->getDoctrine()->getManager();
+    
+        $em->remove($contact);
+        $em->flush();
+
+        // redirige la page
+        return $this->redirectToRoute('home');
+        }   
 }
